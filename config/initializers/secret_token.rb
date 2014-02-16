d4d7a549710d7cb017e1e8b55b1dd6210910976e
Noe De La Cruz <candiapps@gmail.com>
2014-02-15 22:56:00 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BronyBlindBagCheat::Application.config.secret_key_base = 'bad156e1116f946af2b7b2bffe56a6eefbede85d1e0692214e890d40cfd757a80d58918f65dbf640ff3cc55dfbdbaff5fcbfb09532966253ff05a9874066f355'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BronyBlindBagCheat::Application.config.session_store :cookie_store, key: '_brony-blind-bag-cheat_session'
