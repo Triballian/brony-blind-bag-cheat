f9f94a1238b337564fdee2b52a63d4ca8ff412b3
Noe De La Cruz <candiapps@gmail.com>
2014-02-18 02:25:25 -0600

    partial success with new figures form

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,4 +1,2 @@
 class Figure < ActiveRecord::Base
-
-	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140218053502) do
+ActiveRecord::Schema.define(version: 20140218082150) do
+
+  create_table "figures", force: true do |t|
+    t.string   "bagletter"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
