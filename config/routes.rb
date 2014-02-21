b1d55d1f6166e5ddddd89b83272e63937ac5f94f
Noe De La Cruz <candiapps@gmail.com>
2014-02-21 09:45:15 -0600

    Updated the About page

--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,58 @@ BronyBlindBagCheat::Application.routes.draw do
   get 'figures/code_location'
   root to: 'figures#new'
 end
+
+# The priority is based upon order of creation: first created -> highest
+# priority.
+# Se how all your routes lay out with "rake routes"
+
+# You can have the root of your site routed with "root"
+# root to: 'welcome#index'
+
+# Example of regular route:
+# get 'products/:id' => 'catalog#view'
+
+# Example of named route that can be invoked with
+# purchase_url(id: product.id)
+#   get 'prducts/:id/purchase' => 'catalog#purchase', as: :purchase
+
+# Example resource route (map HTTP verbs to controller action
+# automatically):
+#   resources :prducts
+
+# Example resource route with options:
+#   resources :products do
+#     member do 
+#       get 'short'
+#       post 'toggle'
+#     end
+
+# collection do
+# get 'sold'
+# end
+
+# Example resource route with sub-resources:
+# resources :products do
+#    resources :comments, :sales
+#    resources :seller
+# end
+
+# Example resource route with more complex sub-resources:
+#  resources :prducts do
+#    resources :comments
+#    resources :sales do
+#      get 'recent', on: :collection
+#    end
+# end
+
+# Example resources route withing a namespace
+#   namespce :admin do
+#     # Directs /admin/products/* to Admin::ProductsController
+#     # (app/controller/admin/products_controller.rb)
+#     resources :prducts
+#   end
+
+
+
+
+
